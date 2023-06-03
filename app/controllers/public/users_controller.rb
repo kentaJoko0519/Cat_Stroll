67b41458dda97593d3061d85b88f0403a1ca0a33
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-06-03 12:17:46 +0000

    [Cat-Stroll]カルーセルインジケーター修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -47,7 +47,7 @@ class Public::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:first_name_kana,:last_name_kana, :email, :profile_image, :introduction)
+    params.require(:user).permit(:first_name,:last_name,:first_name_kana,:last_name_kana, :email, :profile_image, :introduction, :user_name)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2023_05_26_092140) do
   end
 
   create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
