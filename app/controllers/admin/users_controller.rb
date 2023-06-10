c9b875f1685694d3d83718167e69001f715cadfd
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-06-10 06:47:07 +0000

    [cat_stroll]管理者側 user一覧、詳細、編集機能作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,29 @@
 class Admin::UsersController < ApplicationController
+  # ユーザー一覧
   def index
+    @users = User.all
   end
-
+  
+  # ユーザー詳細 
   def show
+    @user=User.find(params[:id])
+    @posts=@user.posts
   end
-
+  
+  # ユーザー情報編集
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(@user.id)
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:first_name_kana,:last_name_kana, :email, :profile_image, :introduction, :user_name, :is_deleted)
+  end
+  
 end
