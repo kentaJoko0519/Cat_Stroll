73308bb40ab3565a5eee4b4d1cf8dcb84fb79418
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-06-12 12:23:58 +0000

    [cat_stroll]管理者側 投稿閲覧機能作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,18 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    # コメント機能
+    @comments = @post.comments  #投稿詳細に関連付けてあるコメントを全て取得
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path
   end
 end
