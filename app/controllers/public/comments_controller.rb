5d8022265fcd4af091dc668a67d0c0b8c25e3924
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-07-01 09:06:03 +0000

    [cat_stroll]　Issue20 コメント削除機能追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,7 @@
 class Public::CommentsController < ApplicationController
 # 新規登録orログインしないとアクションが実行されない
   before_action :authenticate_user!
-  
+
 
   def create
     @comment = Comment.new(comment_params)
@@ -13,16 +13,16 @@ class Public::CommentsController < ApplicationController
     end
 
   end
-  
+
   def destroy
-    comment = Comment.find(params[:post_id])
+    comment = Comment.find(params[:id])
     comment.destroy
-    redirect_to post_path(params[:post_id])
+    redirect_to post_path(comment.post.id)
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:sentence, :post_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
   end
-  
+
 end
