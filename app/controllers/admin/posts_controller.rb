8e6897e68b789dbd67d3c7af30d7fc411ded6aa5
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-07-09 12:13:55 +0000

    [cat_stroll]　Issue36 投稿一覧の検索機能の修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -20,6 +20,13 @@ class Admin::PostsController < ApplicationController
         @posts = (tag_posts + posts).uniq
       end
     end
+    
+    # 検索結果がなかった場合
+    if params[:search].present? && @posts.count == 0
+      flash[:alert] = "検索結果がありません"
+      render :index
+    end
+    flash[:alert] = nil
   end
   
   def show
