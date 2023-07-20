a2c1e619aa8567c07b3f80595f01724e7f2edf60
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-07-20 11:56:04 +0000

    [cat_strill]　アプリケーション修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
-# 管理者側  ユーザー  
+# 管理者側  ユーザー
   before_action :authenticate_admin!
-  
+
   def index
     @users = User.all
     # 検索機能
@@ -15,19 +15,19 @@ class Admin::UsersController < ApplicationController
         # 重複しないように uniq を設定
         @users = users.uniq
       end
-    end  
+    end
     if params[:search].present? && @users.count == 0
       flash[:alert] = "検索結果がありません"
       render :index
     end
     flash[:alert] = nil
   end
-  
+
   def show
     @user=User.find(params[:id])
     @posts=@user.posts
   end
-  
+
   def edit
     @user=User.find(params[:id])
   end
@@ -38,10 +38,10 @@ class Admin::UsersController < ApplicationController
     flash[:notice] = "登録情報変更完了"
     redirect_to admin_user_path(@user.id)
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:first_name,:last_name,:first_name_kana,:last_name_kana, :email, :profile_image, :introduction, :user_name, :is_deleted)
   end
-  
+
 end
--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,25 +1,25 @@
 class Public::RelationshipsController < ApplicationController
-# ユーザー側  フォロー  
+# ユーザー側  フォロー
   before_action :authenticate_user!
-  
+
   # フォローする
   def create
     current_user.follow(params[:user_id])       # current_user で 選択したユーザー(user_id)に対してfollowメソッドを実行
     redirect_to request.referer                 # 同じ画面に戻る
   end
-  
+
   # フォローを外す
   def destroy
     current_user.unfollow(params[:user_id])     # current_user で 選択したユーザー(user_id)に対してunfollowメソッドを実行
     redirect_to request.referer
   end
-  
+
 # フォロー中
   def following
     @user = User.find(params[:user_id])
     @following_users = @user.following_user
   end
-  
+
 # フォロワー
   def follower
     @user = User.find(params[:user_id])
